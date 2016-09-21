e3c8d0b789b78b25b50e866680be6baae8b31e8c
xenonmolecule <michael_ryan_2000@yahoo.com>
2016-09-21 19:59:50 +0000

    initialized necessary files

---
+++ src/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+//PREPARE BOOTSTRAP STATIC LINK
+app.use('/bootstrap', express.static(__dirname+'/node_modules/bootstrap'));
+//PREPARE STATIC FILES FOR DISTRIBUTION
+app.use('/static', express.static(__dirname + '/public'));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+//CHANGE LATER, HERE FOR C9
+var port = process.env.PORT;
+http.listen(port,function(){
+    console.log("The process is running on port:"+port);
+});
+
+//HOME ROUTE--
+//      Should be what the user sees when they first arrive on the website
+app.get("/",function(req,res){
+    res.render("home.jade");
+});
